50b8a006d0f23921b67cf1a1ff9ab2526c382634
Philippe Teuwen <phil@teuwen.org>
2016-06-12 22:14:53 +0200

    openwb: wrong src file

--- wbs_aes_openwhitebox_chow/target/encryptECB.go
+++ wbs_aes_openwhitebox_chow/target/encryptECB.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
+
 	"github.com/OpenWhiteBox/AES/constructions/chow"
 )
 
+var (
+	key = flag.String("key", "key.txt", "White-Box AES Key to encrypt with.")
+	in  = flag.String("in", "secrets.txt", "Input file to encrypt.")
+	out = flag.String("out", "secrets.enc", "Output file to write the encrypted file to.")
+)
+
 func main() {
-	keyData, _ := ioutil.ReadFile("key.txt")
+	flag.Parse()
+
+	// Load in key data and initialize the block cipher.
+	keyData, err := ioutil.ReadFile(*key)
+	if err != nil {
+		panic(err)
+	}
+
 	block, _ := chow.Parse(keyData)
-	data, _ := ioutil.ReadFile("secrets.txt")
+
+	// Load in file to encrypt.
+	data, err := ioutil.ReadFile(*in)
+	if err != nil {
+		panic(err)
+	}
+
+	// Encrypt file.
 	block.Encrypt(data,data)
-	ioutil.WriteFile("secrets.enc", data, os.ModePerm)
+
+	// Write encrypted file to disk.
+	ioutil.WriteFile(*out, data, os.ModePerm)
+
+	fmt.Println("Done!")
 }
