59c523f5eabdb900b661cc44325eec30a71bad02
Charles Hubain <github@haxelion.eu>
2016-11-12 20:41:21 +0100

    Default wyseur to tracergrind tracing because of the pin sysenter bug

--- wbs_des_wyseur2007/DCA/trace_it.py
+++ wbs_des_wyseur2007/DCA/trace_it.py
@@ -11,8 +11,8 @@ def processinput(iblock, blocksize):
 def processoutput(output, blocksize):
     return int(''.join([x for x in output.split('\n') if x.find('OUTPUT')==0][0][10:].split(' ')), 16)
 
-T=TracerPIN('../target/wbDES', processinput, processoutput, ARCH.i386, 8)
-#T=TracerGrind('../target/wbDES', processinput, processoutput, ARCH.i386, 8, addr_range='0x08000000-0x08100000')
+#T=TracerPIN('../target/wbDES', processinput, processoutput, ARCH.i386, 8)
+T=TracerGrind('../target/wbDES', processinput, processoutput, ARCH.i386, 8, addr_range='0x08000000-0x08100000')
 T.run(100)
 bin2daredevil(config={'algorithm':'DES', 'position':'LUT/DES_SBOX', 'correct_key':'0x30 32 34 32 34 36 32 36'})
 #bin2daredevil(configs={'attack_sbox': {'algorithm':'DES', 'position':'LUT/DES_SBOX', 'correct_key':'0x30 32 34 32 34 36 32 36'},
