cc0aef8ff49b169bb9ee65fda3846d5f4fe680d4
Philippe Teuwen <phil@teuwen.org>
2016-11-21 01:55:34 +0100

    DFA on wbs_aes_ches2016 now optimized

--- wbs_aes_ches2016/DFA/attack_ches2016.py
+++ wbs_aes_ches2016/DFA/attack_ches2016.py
@@ -12,8 +12,15 @@ def processoutput(output, blocksize):
     i=int(b''.join([x for x in output.split(b'\n') if x.find(b'OUTPUT')==0][0][10:].split(b' ')), 16)
     return i
 
+# Initial attack attempt:
+#engine=deadpool_dfa.Acquisition(targetbin='./wb_challenge', targetdata='./wb_challenge', goldendata='./wb_challenge.gold',
+#        dfa=phoenixAES, processinput=processinput, processoutput=processoutput, addresses=[0x21aa0,0xa0ac0], minfaultspercol=200)
+# Because of many false positives, we had to extend minfaultspercol to capture more traces
+# But looking at the resulting *.log (cut -c 10-30 *.log |sort|uniq) we see some distinct address ranges producing faults with the right pattern.
+# So we can target one of those address ranges and reduce the number of desired traces to the default for a much faster and determinitic attack:
+
 engine=deadpool_dfa.Acquisition(targetbin='./wb_challenge', targetdata='./wb_challenge', goldendata='./wb_challenge.gold',
-        dfa=phoenixAES, processinput=processinput, processoutput=processoutput, addresses=[0x21aa0,0xa0ac0], minfaultspercol=200)
+        dfa=phoenixAES, processinput=processinput, processoutput=processoutput, addresses=[0x3bab0,0x3d300])
 tracefiles=engine.run()
 for tracefile in tracefiles:
     if phoenixAES.crack(tracefile):
