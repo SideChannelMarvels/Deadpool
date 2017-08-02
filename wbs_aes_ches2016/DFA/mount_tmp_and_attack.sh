c6c603aa0c3bee8dbde80d6e9bc93d673967fa6e
Philippe Teuwen <phil@teuwen.org>
2017-08-03 00:06:17 +0200

    ches2016 DFA: propose precompiled chal by default

--- wbs_aes_ches2016/DFA/mount_tmp_and_attack.sh
+++ wbs_aes_ches2016/DFA/mount_tmp_and_attack.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+
+# Compile the challenge from scratch or use the precompiled binary?
+# Problems may arise when you compile it yourself because addresses may have changed
+# and you will need to tune attack_ches2016.py "addresses=[0x3b000,0x3f000]" accordingly
+# (or just remove that parameter and go for a slower attack)
+USE_PRECOMPILED_CHALLENGE=true
+
 mkdir -p tmp
 mount|grep -q $(pwd)/tmp || sudo mount -t tmpfs -o mode=01777,size=20m tmpfs tmp
 cp attack_ches2016.py ../../deadpool_dfa.py ../../../JeanGrey/phoenixAES.py tmp
-cp ../target/*.[ch] ../target/Makefile tmp
+$USE_PRECOMPILED_CHALLENGE || cp ../target/*.[ch] ../target/Makefile tmp
+$USE_PRECOMPILED_CHALLENGE && cp ../target/wb_challenge tmp
 cd tmp
-make
+$USE_PRECOMPILED_CHALLENGE || make
 cp -a wb_challenge wb_challenge.gold
 ./attack_ches2016.py
