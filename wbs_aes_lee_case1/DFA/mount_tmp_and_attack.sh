20c00ac6b771f24e3659fb6013a8f3b4e1167c68
Philippe Teuwen <phil@teuwen.org>
2017-09-09 20:14:54 +0200

    WB_LEE_CASE1 update

--- wbs_aes_lee_case1/DFA/mount_tmp_and_attack.sh
+++ wbs_aes_lee_case1/DFA/mount_tmp_and_attack.sh
@@ -3,8 +3,7 @@
 mkdir -p tmp
 mount|grep -q $(pwd)/tmp || sudo mount -t tmpfs -o mode=01777,size=20m tmpfs tmp
 cp attack_lee_case1.py ../../deadpool_dfa*.py ../../../JeanGrey/phoenixAES.py tmp
-cp ../target/WB_LEE_CASE1_Client ../target/Tab.bin tmp
-cp ../target/WB_LEE_CASE1_Client tmp/WB_LEE_CASE1_Client.gold
+cp ../target/WB_LEE_CASE1_Client tmp
 cp ../target/Tab.bin tmp/Tab.bin.gold
 cd tmp
 ./attack_lee_case1.py
