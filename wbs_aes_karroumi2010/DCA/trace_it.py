1e484d535e2862ef9d657d4449123683589dcc17
Philippe Teuwen <phil@teuwen.org>
2018-04-30 17:57:21 +0200

    Karroumi & TracerGrind

--- wbs_aes_karroumi2010/DCA/trace_it.py
+++ wbs_aes_karroumi2010/DCA/trace_it.py
@@ -14,8 +14,11 @@ filters=[Filter('mem_addr1_rw1', ['R', 'W'], lambda stack_range, addr, size, dat
 #filters=[Filter('mem_addr1_rw1', ['R', 'W'], lambda stack_range, addr, size, data: addr < 0x7ffffff68000 and size == 1, lambda addr, size, data: addr & 0xFF, '<B'),
 #         Filter('mem_data_rw1',  ['R', 'W'], lambda stack_range, addr, size, data: addr < 0x7ffffff68000 and size == 1, lambda addr, size, data: data, '<B')]
 
-# Beware if you adapt the script to use the version compiled for Boost 1.55, you'll have to adapt the address range too!
-T=TracerPIN('../target/main', processinput, processoutput, ARCH.amd64, 16, addr_range='0x4676d0-0x468eed', filters=filters, shell=True)
+# Beware if you adapt the script to use other Boost versions, you'll have to adapt the address range too!
+# TracerPIN and libboost 1.58:
+#T=TracerPIN('../target/main_boost1.58', processinput, processoutput, ARCH.amd64, 16, addr_range='0x4676d0-0x468eed', filters=filters, shell=True)
+# TracerGrind and libboost 1.62:
+T=TracerGrind('../target/main_boost1.62', processinput, processoutput, ARCH.amd64, 16, addr_range='0x178000-0x17b700', filters=filters, shell=True)
 T.run(2000)
 bin2daredevil(keywords=filters,
               configs={'attack_sbox':   {'algorithm':'AES', 'position':'LUT/AES_AFTER_SBOX',    'correct_key':'0x2b7e151628aed2a6abf7158809cf4f3c'},
--- wbs_aes_karroumi2010/traces/README.md
+++ wbs_aes_karroumi2010/traces/README.md
@@ -1,6 +1,6 @@
 ## Usage
 
-## Acquiring one trace with TracerPIN
+## Acquiring one trace with TracerPIN on the Boost 1.58 version
 
 
 ```bash
@@ -19,4 +19,15 @@ tracegraph trace-full-info.sqlite &
 
 We can see most AES rounds occur somewhere between 0x4676d0 and 0x468eed with the stack beyond 0x7ffffff68000.
 
-Note that if you're using the version compiled for Boost 1.55, address range will vary.
+## Acquiring and visualiszing one trace with TracerGrind on the Boost 1.62 version
+
+
+```bash
+valgrind --tool=tracergrind --output=trace.grind --filter=0x000000-0x400000 ./main --load-tables tables_karroumi_noextenc.tbl --input-files <(echo 000102030405060708090a0b0c0d0e0f|xxd -r -p) --out-file >(xxd -p)
+sqlitetrace trace.grind trace.sqlite
+tracegraph trace.sqlite &
+```
+
+We can see most AES rounds occur somewhere between 0x178000 and 0x17b700 with the stack beyond 0xffef6a000.
+
+Note that if you're using other Boost versions, address range will vary.
