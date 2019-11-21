500675bd2f7dbed4721a38c8b597ea5090f9b0d8
Philippe Teuwen <phil@teuwen.org>
2019-11-21 10:21:36 +0100

    fix gh19 dca scripts

--- wbs_aes_grehack2019/DCA/00_instrument_it.sh
+++ wbs_aes_grehack2019/DCA/00_instrument_it.sh
@@ -3,7 +3,8 @@
 (
   cd ../target
   tar xf 1573854749.4296393_e6b19a0f2ddea947e2166ddf9c7fa852e93cd3f4.tar.gz
-  sed 's/instr\.c/instr_instrumented.c/' main.c > main_instrumented.c
+  sed 's/enc\.c/enc_instrumented.c/' main.c > main_instrumented.c
+  sed 's/instr\.c/instr_instrumented.c/' enc.c > enc_instrumented.c
   sed 's/;/;\n/g' instr.c > instr_instrumented.c
   sed -i 's/s\[\([0-9]\+\)\]=\(.*\);/s[\1]=\2;fprintf(stderr, "%c", s[\1]);/' instr_instrumented.c
   gcc -o main_instrumented main_instrumented.c
--- wbs_aes_grehack2019/DCA/break_it_all.sh
+++ wbs_aes_grehack2019/DCA/break_it_all.sh
@@ -6,7 +6,8 @@
 
 (
   cd ../target
-  sed 's/instr\.c/instr_instrumented.c/' main.c > main_instrumented.c
+  sed 's/enc\.c/enc_instrumented.c/' main.c > main_instrumented.c
+  sed 's/instr\.c/instr_instrumented.c/' enc.c > enc_instrumented.c
   sed 's/;/;\n/g' instr.c > instr_instrumented.c
   sed -i 's/s\[\([0-9]\+\)\]=\(.*\);/s[\1]=\2;fprintf(stderr, "%c", s[\1]);/' instr_instrumented.c
   gcc -o main_instrumented main_instrumented.c
