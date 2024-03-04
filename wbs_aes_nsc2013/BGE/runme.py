62320360339a521ab14c723d7ded1fae40331028
Philippe Teuwen <phil@teuwen.org>
2024-03-04 23:19:09 +0100

    Adapt BGE against wbs_aes_nsc2013 to use BlueGalaxyEnergy 2.0.0

--- wbs_aes_nsc2013/BGE/runme.py
+++ wbs_aes_nsc2013/BGE/runme.py
@@ -3,23 +3,25 @@
 from bluegalaxyenergy import BGE
 from nosuchcon_2013_whitebox import NSCWhiteBoxedAES
 
+
 def test():
     mywb = NSCWhiteBoxedAES()
 
     # run the attack
     bge = BGE(mywb)
-    bge.run(roundList=[2,3,4,5])
+    # Direct attempt fails with an advise to use 'run(..., shuffle=True)'
+    # bge.run(roundList=[2, 3, 4, 5])
+    bge.run(roundList=[2, 3, 4, 5], shuffle=True)
 
     # extract the key from the available roundKeys
     key = bge.computeKey()
     if key is not None:
         print("key:", key.hex())
-    # for this specific whitebox, returned roundkeys were transposed
-    # therefore if at least two roundkeys were found, previous computeKey() failed
-    # and we need to indicate the round keys were transposed:
-    key = bge.computeKey(transposed_rk=True)
-    if key is not None:
-        print("key:", key.hex())
+        # State was actually not shuffled but simply transposed:
+        print("states shufflings:")
+        for k, v in bge.getShuffle().items():
+            print(k, v)
+
 
 if __name__ == "__main__":
     test()
